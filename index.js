8a3f1565a48f52504e0d102943273ca1acdd30e4
Gabriel Farrell <g@grrawr.com>
2013-05-29 01:45:31 -0400

    Simplified main

--- index.js
+++ index.js
@@ -1,91 +1,34 @@
-module.exports = Stack;
-var Url = require('url');
+function stack (/*layers*/) {
+  // Innermost (last) handler is always 404
+  var handler = stack.notFoundHandler
 
-function Stack(/*layers*/) {
-  var error = Stack.errorHandler,
-      handle = error;
   Array.prototype.slice.call(arguments).reverse().forEach(function (layer) {
-    var child = handle;
-    handle = function (req, res) {
+    var child = handler
+    handler = function (req, res) {
       try {
         layer(req, res, function (err) {
-          if (err) { return error(req, res, err); }
-          child(req, res);
-        });
+          if (err) { 
+            return stack.errorHandler(req, res, err) 
+          }
+          child(req, res)
+        })
       } catch (err) {
-        error(req, res, err);
+        stack.errorHandler(req, res, err)
       }
-    };
-  });
-  return handle;
-}
-Stack.errorHandler = function error(req, res, err) {
-  if (err) {
-    console.error(err.stack);
-    res.writeHead(500, {"Content-Type": "text/plain"});
-    res.end(err.stack + "\n");
-    return;
-  }
-  res.writeHead(404, {"Content-Type": "text/plain"});
-  res.end("Not Found\n");
-};
-
-function core(req, res, next) { next(); }
-
-// Build a composite stack made of several layers
-Stack.compose = function compose(/*layers*/) {
-  // Don't bother composing singletons
-  if (arguments.length == 1) { return arguments[0]; }
-
-  var stack = core;
-  Array.prototype.slice.call(arguments).reverse().forEach(function (layer) {
-
-    var child = stack;
-    stack = function (req, res, next) {
-      try {
-        layer(req, res, function (err) {
-          if (err) { return next(err); }
-          child(req, res, next);
-        });
-      } catch (err) {
-        next(err);
-      }
-    };
-
-  });
-
-  return stack;
+    }
+  })
+  return handler
 }
 
-// Mounts a substack app at a url subtree
-Stack.mount = function mount(mountpoint/*, stack*/) {
+stack.errorHandler = function (req, res, err) {
+  console.error(err.stack)
+  res.writeHead(500, {'Content-Type': 'text/plain'})
+  return res.end('Error\n')
+}
 
-  var stack = Stack.compose.apply(null, Array.prototype.slice.call(arguments, 1));
+stack.notFoundHandler = function (req, res) {
+  res.writeHead(404, {'Content-Type': 'text/plain'})
+  res.end('Not Found\n')
+}
 
-  if (mountpoint.substr(mountpoint.length - 1) == "/") {
-    mountpoint = mountpoint.substr(0, mountpoint.length - 1);
-  }
-
-  var matchpoint = mountpoint + "/";
-
-  return function (req, res, next) {
-    var url = req.url;
-    var uri = req.uri;
-
-    if (url.substr(0, matchpoint.length) !== matchpoint) { return next(); }
-
-    // Modify the url
-    if (!req.realUrl) { req.realUrl = url; }
-
-    req.url = url.substr(mountpoint.length);
-    if (req.uri) { req.uri = Url.parse(req.url); }
-
-    stack(req, res, function (err) {
-      req.url = url;
-      req.uri = uri;
-      next(err)
-    });
-
-  };
-
-};
+module.exports = stack
